59bf14b07905387cf6aa4abc8fd8d7f97769e538
carloscalderon <cdcalderon@gmail.com>
2017-03-05 15:48:54 -0600

    change to use heroku backend

--- src/app/pages/stock-earnings/stock.earnings.service.ts
+++ src/app/pages/stock-earnings/stock.earnings.service.ts
@@ -8,8 +8,9 @@ import { IStockEarning } from './stockEarning';
 
 @Injectable()
 export class StockEarningsService {
-  //private _stockEarningsUrl = 'https://gentle-everglades-23329.herokuapp.com/earnings';
-  private _stockEarningsUrl = 'http://localhost:4000/earnings?callback=JSONP_CALLBACK';
+  private _stockEarningsUrl = 'https://gentle-everglades-23329.herokuapp.com/earnings';
+  //private _stockEarningsUrl = 'http://localhost:4000/earnings?callback=JSONP_CALLBACK';
+  //private _stockEarningsUrl = 'http://localhost:4000/earnings';
   constructor(private _http: Http) { }
 
   getStockEarnings(): Observable<IStockEarning[]> {
